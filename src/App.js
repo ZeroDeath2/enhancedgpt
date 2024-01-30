a8dfb566ed2469b8222fc2fcbd1d085331f489e7
ZeroDeath2 <harinchirappannath@gmail.com>
2024-01-30 08:27:21 +0530

    styling and api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import gpt3 from './gpt3.png';
 
 function App() {
   return (
@@ -12,6 +13,25 @@ function App() {
         </section>
       </aside>
       <section className='chatbox'>
+        <div className='chat-message'>
+          <div className='chatbox-log'>
+            <div className='avatar'>
+                  
+            </div>
+            <div className='message'>
+              Hello world
+          </div>
+          </div>
+        </div><div className='gpt-message'>
+          <div className='chatbox-log'>
+            <div className='gptavatar'>
+                  <image src={gpt3} alt='gpt3' />
+            </div>
+            <div className='message'>
+              I am a bot
+          </div>
+          </div>
+        </div>
       <div className='chat-input-holder'>
         <textarea className='chat-input-area' rows="1" placeholder='Type your message here...'></textarea>
       </div>
